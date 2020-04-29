c8d29c137315a992d071d88785f91edf4239284e
alfredo reichlin <alfredoreichlin@MacBook-Pro-di-alfredo.local>
2020-04-29 11:24:51 +0200

    A network testing

--- experiments_scripts/search_stable_policy.sh
+++ experiments_scripts/search_stable_policy.sh
@@ -1,8 +1,8 @@
 
 #! /bin/bash
 
-rm -r /Midgard/home/pshi/alfredo/experiments_scripts/slurm*
-rm -r /Midgard/home/pshi/alfredo/experiments_scripts/policy_log/eps*
+#rm -r /Midgard/home/pshi/alfredo/experiments_scripts/slurm*
+#rm -r /Midgard/home/pshi/alfredo/experiments_scripts/policy_log/eps*
 
 sbatch --export=BETA=0.1,SIZE=32,DEPTH=6,JOIN=1,ACT=1 search_stable_policy.sbatch
 sleep 1
@@ -14,10 +14,10 @@ sbatch --export=BETA=0.1,SIZE=64,DEPTH=6,JOIN=1,ACT=0 search_stable_policy.sbatc
 sleep 1
 sbatch --export=BETA=0.1,SIZE=64,DEPTH=8,JOIN=1,ACT=0 search_stable_policy.sbatch
 sleep 1
-sbatch --export=BETA=0.01,SIZE=32,DEPTH=6,JOIN=1,ACT=0 search_stable_policy.sbatch
-sleep 1
-sbatch --export=BETA=10.0,SIZE=32,DEPTH=6,JOIN=1,ACT=0 search_stable_policy.sbatch
-sleep 1
+#sbatch --export=BETA=0.01,SIZE=32,DEPTH=6,JOIN=1,ACT=0 search_stable_policy.sbatch
+#sleep 1
+#sbatch --export=BETA=10.0,SIZE=32,DEPTH=6,JOIN=1,ACT=0 search_stable_policy.sbatch
+#sleep 1
 
 #for eps in 0.5 0.2 0.05 0.01; do
 #  for decay in 0.25 0.4 0.64; do
